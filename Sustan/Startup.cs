d1b102c7654422125664898bcd23c02ee4a991c0
Vlada Krstin <krstinv@gmail.com>
2018-04-27 19:53:36 +0200

    Add project files.

---
+++ Sustan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sustan.Startup))]
+namespace Sustan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
